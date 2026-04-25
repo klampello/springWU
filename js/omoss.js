c7f00201277fc2fc70c56d18fb8c441348977c90
klampello <adam.svennevid@elev.ga.ntig.se>
2026-04-25 11:58:04 +0200

    Biggest push of all time

---
+++ js/omoss.js
@@ -0,0 +1,7 @@
+function openContact() {
+  document.getElementById("contact-panel").classList.add("open");
+}
+
+function closeContact() {
+  document.getElementById("contact-panel").classList.remove("open");
+}
